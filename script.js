5166d11ec54ebf37956a96e05caeaabb14dd10fb
VINICIUS RODRIGUES GABRIELLI <vinicius_gabrielli1@estudante.sesisenai.org.br>
2024-03-14 10:14:45 -0300

    teste

---
+++ script.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById('meuCanvas');
+let ctx = canvas.getContext('2d');
+
+let img = new Image();
+img.onload = function(){
+  ctx.drawImage(img,0,0,canvas.clientWidth, canvas.height);
+};
+img.src = './assets/normal.png'
